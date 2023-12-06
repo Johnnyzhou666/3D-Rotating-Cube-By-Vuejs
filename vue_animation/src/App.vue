51fba18ffdc9203c5be4fd3db1bd902b30e6ac2e
Johnnyzhou666 <140654100+Johnnyzhou666@users.noreply.github.com>
2023-12-05 22:41:41 -0500

    Update App.vue

--- vue_animation/src/App.vue
+++ vue_animation/src/App.vue
@@ -4,20 +4,20 @@
 
     <div class="wrapper">
         
-        <input class="radio-button" type="radio" name="cube-image" checked>
-        <input class="radio-button" type="radio" name="cube-image">
-        <input class="radio-button" type="radio" name="cube-image">
-        <input class="radio-button" type="radio" name="cube-image">
-        <input class="radio-button" type="radio" name="cube-image">
-        <input class="radio-button" type="radio" name="cube-image">
+        <input class="rotation-button" type="radio" name="cube-image" checked>
+        <input class="rotation-button" type="radio" name="cube-image">
+        <input class="rotation-button" type="radio" name="cube-image">
+        <input class="rotation-button" type="radio" name="cube-image">
+        <input class="rotation-button" type="radio" name="cube-image">
+        <input class="rotation-button" type="radio" name="cube-image">
     
         <div class="cube">
-            <div class="cube-side"></div>
-            <div class="cube-side"></div>
-            <div class="cube-side"></div>
-            <div class="cube-side"></div>
-            <div class="cube-side"></div>
-            <div class="cube-side"></div>
+            <div class="cube-face"></div>
+            <div class="cube-face"></div>
+            <div class="cube-face"></div>
+            <div class="cube-face"></div>
+            <div class="cube-face"></div>
+            <div class="cube-face"></div>
         </div>
     </div>
    
@@ -57,7 +57,7 @@ body {
     transition-duration: 2s;
 }
 
-.cube-side {
+.cube-face {
     position: absolute;
     width: 100%;
     height: 100%;
@@ -68,57 +68,57 @@ body {
     
 }
 
-.radio-button {
+.rotation-button {
     margin-bottom: 6em;
 }
 
-.cube-side:nth-child(1) {
+.cube-face:nth-child(1) {
     transform: rotateY(0deg) translateZ(7.5em);
     background-image:url(1.jpg);
 
 }
-.cube-side:nth-child(2) {
+.cube-face:nth-child(2) {
     transform: rotateY(90deg) translateZ(7.5em);
     background-image: url(2.JPG);
 }
-.cube-side:nth-child(3) {
+.cube-face:nth-child(3) {
     transform: rotateY(180deg) translateZ(7.5em);
     background-image: url(3.JPG);
 }
-.cube-side:nth-child(4) {
+.cube-face:nth-child(4) {
     transform: rotateY(-90deg) translateZ(7.5em);
     background-image: url(4.JPG);
 }
-.cube-side:nth-child(5) {
+.cube-face:nth-child(5) {
     transform: rotateX(-90deg) translateZ(7.5em);
     background-image: url(5.jpg);
 }
-.cube-side:nth-child(6) {
+.cube-face:nth-child(6) {
     transform: rotateX(90deg) translateZ(7.5em);
     background-image: url(6.JPG);
 }
 
 
-.radio-button:checked ~ .cube {
+.rotation-button:checked ~ .cube {
     transition-duration: 3s;
     transition-timing-function: cubic-bezier(0.19, 1, 0.22, 1);
 }
-.radio-button:nth-child(1):checked ~ .cube { 
+.rotation-button:nth-child(1):checked ~ .cube { 
     transform: rotateX(-15deg) rotateY(0deg);
 }
-.radio-button:nth-child(2):checked ~ .cube { 
+.rotation-button:nth-child(2):checked ~ .cube { 
     transform: rotateX(-15deg) rotateY(180deg);
 }
-.radio-button:nth-child(3):checked ~ .cube { 
+.rotation-button:nth-child(3):checked ~ .cube { 
     transform: rotateX(-15deg) rotateY(90deg);
 }
-.radio-button:nth-child(4):checked ~ .cube { 
+.rotation-button:nth-child(4):checked ~ .cube { 
     transform: rotateX(-15deg) rotateY(-90deg);
 }
-.radio-button:nth-child(5):checked ~ .cube { 
+.rotation-button:nth-child(5):checked ~ .cube { 
     transform: rotateX(-105deg) rotateY(0deg);
 }
-.radio-button:nth-child(6):checked ~ .cube { 
+.rotation-button:nth-child(6):checked ~ .cube { 
     transform: rotateX(75deg) rotateY(0deg);
 }
 </style>
